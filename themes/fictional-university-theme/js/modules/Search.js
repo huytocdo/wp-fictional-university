53eb6976db5d1c875671f620ff10b57934931ccd
huytocdo <ndhuyspkt@gmail.com>
2019-01-14 23:03:27 +0700

    hoàn thành chương 15

---
+++ themes/fictional-university-theme/js/modules/Search.js
@@ -0,0 +1,116 @@
+import $ from 'jquery';
+
+class Search {
+  // 1. describe and create/initiate our object
+  constructor() {
+    this.addSearchHTML();
+    this.openButton = $(".js-search-trigger");
+    this.closeButton = $(".search-overlay__close");
+    this.searchOverlay = $(".search-overlay");
+    this.searchField = $("#search-term");
+    this.resultsDiv = $(".search-overlay__results");
+    this.isOverlayOpen = false;
+    this.isSpinnerVisible = false;
+    this.previousValue;
+    this.typingTimer;
+    this.events();
+  }
+  // 2. events
+  events() {
+    this.openButton.on("click", this.openOverlay.bind(this));
+    this.closeButton.on("click", this.closeOverlay.bind(this));
+    $(document).on("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.on("keyup", this.typingLogic.bind(this));
+  }
+
+  // 3. methods (function, action...)
+  typingLogic() {
+    if(this.searchField.val() !== this.previousValue) {
+      clearTimeout(this.typingTimer);
+      if(this.searchField.val()) {
+        if(!this.isSpinnerVisible) {
+          this.resultsDiv.html('<div class="spinner-loader"></div>');
+          this.isSpinnerVisible = true;
+        }
+        this.typingTimer = setTimeout(this.getResults.bind(this), 750); 
+      } else {
+        this.resultsDiv.html("");
+        this.isSpinnerVisible = false;
+      }
+    }
+    this.previousValue = this.searchField.val(); 
+  }
+
+  getResults() {
+    $.when(
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search='+this.searchField.val()),
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search='+this.searchField.val())
+    ).then((posts, pages) => {
+      var combinedResults = posts[0].concat(pages[0]);
+      this.resultsDiv.html(`
+        <h2 class="search-overlay__section-title">General Information</h2>
+        ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information that match the keywords</p>'}
+          ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a> ${item.authorName ? 'by ' + item.authorName : ''}</li>`).join('')}
+        ${combinedResults.length ? '</ul>' : ''}
+      `);
+      this.isSpinnerVisible = false;
+    }, (error) => {
+      this.resultsDiv.html('<p>Unexpected error; please try again!</p>');
+    })
+  }
+
+  keyPressDispatcher(e) {
+    if(!$("input, textarea").is(':focus')) {
+      switch (e.keyCode) {
+        case 83:
+          if(!this.isOverlayOpen) {
+            this.openOverlay();
+          }
+          break;
+        case 27:
+          if(this.isOverlayOpen) {
+            this.closeOverlay();
+          }
+          break;
+      }
+    }
+  }
+
+  openOverlay() {
+    this.searchOverlay.addClass("search-overlay--active");
+    $("body").addClass("body-no-scroll");
+    this.searchField.val('');
+    setTimeout(() => {
+      this.searchField.focus();
+    }, 301)
+    this.isOverlayOpen = true;
+  }
+
+  closeOverlay() {
+    this.searchOverlay.removeClass("search-overlay--active");
+    $("body").removeClass("body-no-scroll");
+    this.isOverlayOpen = false;
+  }
+
+  addSearchHTML() {
+    $("body").append(`
+      <div class="search-overlay">
+        <div class="search-overlay__top">
+          <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="container">
+          <div class="search-overlay__results">
+            
+          </div>
+        </div>
+      </div>
+    `);
+  }
+
+}
+
+export default Search;
