e76155d1f632cc7581c398bb7490a8cf37d45c43
huytocdo <ndhuyspkt@gmail.com>
2019-01-27 23:23:22 +0700

    hoàn thành chương 20

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -34,6 +34,7 @@
     ));
     // Professor Post Type
     register_post_type('professor', array(
+      'show_in_rest' => true,
       'supports' => array('title', 'editor', 'thumbnail'),
       'public' => true,
       'labels' => array(
@@ -62,5 +63,37 @@
       ),
       'menu_icon' => 'dashicons-location-alt'
     ));
+    // Note Post Type
+    register_post_type('note', array(
+      'capability_type' => 'note',
+      'map_meta_cap' => true,
+      'show_in_rest' => true,
+      'supports' => array('title', 'editor', 'author'),
+      'public' => false,
+      'show_ui' => true,
+      'labels' => array(
+        'name' => 'Notes',
+        'add_new_item' => 'Add New Note',
+        'edit_item' => 'Edit Note',
+        'all_items' => 'All Notes',
+        'singular_name' => 'Note'
+      ),
+      'menu_icon' => 'dashicons-welcome-write-blog'
+    ));
+    // Like Professor Type
+    register_post_type('like', array(
+      'supports' => array('title', 'author'),
+      'public' => false,
+      'show_ui' => true,
+      'labels' => array(
+        'name' => 'Likes',
+        'add_new_item' => 'Add New Like',
+        'edit_item' => 'Edit Like',
+        'all_items' => 'All Likes',
+        'singular_name' => 'Like'
+      ),
+      'menu_icon' => 'dashicons-heart'
+    ));
   }
+  
   add_action('init', 'university_post_types');
---
+++ themes/fictional-university-theme/js/modules/Like.js
@@ -0,0 +1,69 @@
+import $ from 'jquery';
+
+class Like {
+  constructor() {
+    this.events();
+  }
+
+  events() {
+    $(".like-box").on("click", this.ourClickDispatcher.bind(this));
+  }
+
+  // Methods
+  ourClickDispatcher(e) {
+    var currentLikeBox = $(e.target).closest(".like-box");
+    if(currentLikeBox.attr('data-exists') === 'yes') {
+      this.deleteLike(currentLikeBox);
+    } else {
+      this.createLike(currentLikeBox);
+    }
+  }
+
+  createLike(currentLikeBox) {
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+      },
+      url: universityData.root_url + '/wp-json/university/v1/manageLike',
+      type: 'POST',
+      data: {
+        'professorId': currentLikeBox.data('professor')
+      },
+      success: (res) => {
+        currentLikeBox.attr('data-exists', 'yes');
+        var likeCount = parseInt(currentLikeBox.find(".like-count").html(), 10);
+        likeCount++;
+        currentLikeBox.find(".like-count").html(likeCount);
+        currentLikeBox.attr("data-like", res);
+        console.log('[SUCCESS]', res);
+      },
+      error: (res) => {
+        console.log('[ERROR]', res);
+      }
+    })
+  }
+
+  deleteLike(currentLikeBox) {
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+      },
+      url: universityData.root_url + '/wp-json/university/v1/manageLike',
+      data: {'like': currentLikeBox.attr('data-like')},
+      type: 'DELETE',
+      success: (res) => {
+        currentLikeBox.attr('data-exists', 'no');
+        var likeCount = parseInt(currentLikeBox.find(".like-count").html(), 10);
+        likeCount--;
+        currentLikeBox.find(".like-count").html(likeCount);
+        currentLikeBox.attr("data-like", '');
+        console.log('[SUCCESS]', res);
+      },
+      error: (res) => {
+        console.log('[ERROR]', res);
+      }
+    })
+  }
+}
+
+export default Like;
